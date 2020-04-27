02023109745dd614acd45b7f378388af078c4784
devdrums <devdrums@devdrumss-MBP.fios-router.home>
2020-04-27 09:56:05 -0400

    added post route: athlete

--- app/models/athletes.js
+++ app/models/athletes.js
@@ -2,6 +2,16 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var AthletesSchema = new Schema({
-	firstName: {type: String, lowercase: true, required: true},
-	lastName: {type: String, lowercase: true, required: true},
+	firstName: { type: String, lowercase: true, required: true },
+	lastName: { type: String, lowercase: true, required: true },
+	dateOfBirth: { type: Date, default: Date.now() },
+	gender: { type: String, lowercase: true, required: true },
+	location: { type: String, lowercase: true, required: true },
+	association: { type: String, required: true },
+	sports: { type: Array },
+	team: { type: String },
+	drinksAlcohol: { type: String },
+	married: { type: String },
 });
+
+module.exports = mongoose.model("Athlete", AthletesSchema);
--- server.js
+++ server.js
@@ -1,17 +1,45 @@
 var express = require("express");
 var morgan = require("morgan");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 var app = express();
 var port = process.env.PORT || 8081;
 
+var Athlete = require("./app/models/athletes");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+// To remove deprecation warnings..
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
 
 mongoose.connect("mongodb://localhost:27017/admin", err => {
 	if (err) {
 		console.log(`Not connected to db: ${err}`);
 	} else {
-		console.log("Successfully connected db");
+		console.log("Successfully connected MongoDB");
 	}
+});
+
+app.post("/createProfile", (req, res) => {
+	var athlete = new Athlete();
+
+	athlete.firstName = req.body.firstName;
+	athlete.lastName = req.body.lastName;
+	athlete.dateOfBirth = req.body.dateOfBirth;
+	athlete.gender = req.body.gender;
+	athlete.location = req.body.location;
+	athlete.association = req.body.association;
+	athlete.sports = req.body.sports;
+	athlete.team = req.body.team;
+	athlete.drinksAlcohol = req.body.drinksAlcohol;
+	athlete.married = req.body.married;
+
+	athlete.save();
+
+	res.send("Successfully created profile");
 });
 
 app.listen(port, () => {
